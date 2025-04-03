7479e34904c4a2d0ad213a642d3a833735800cd2
bmichals <benjamin_michals@brown.edu>
2025-04-02 22:42:04 -0400

    Fix Netlify deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   poweredByHeader: false,
   reactStrictMode: true,
+  
+  // Improve Netlify compatibility
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', '*.netlify.app', '*.netlify.com'],
     },
   },
+  
+  // Use by Netlify adapter
+  distDir: '.next',
 };
 
 export default nextConfig;
