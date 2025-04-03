97e9488e54207679a76e694ded1dafe1a28f3546
bmichals <benjamin_michals@brown.edu>
2025-04-02 22:58:12 -0400

    Fix site configuration JSON syntax

---
+++ src/lib/site-config.ts
@@ -0,0 +1,12 @@
+// Site configuration
+export const siteInfo = {
+  id: "museroom",
+  name: "MuseRoom",
+  description: "The Future of Music Creation",
+  domain: "museroomlanding.netlify.app",
+  url: "https://museroomlanding.netlify.app",
+  socials: {
+    twitter: "https://twitter.com/museroom",
+    github: "https://github.com/bmichals25/MR-website",
+  }
+}; 
